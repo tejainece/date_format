c4d139b3cc535437e4cb0742873570e39b81ae55
Jorge Noguera <yoryer@gmail.com>
2019-11-08 16:09:15 -0300

    Added locale support for Spanish

---
+++ lib/src/locale/spanish.dart
@@ -0,0 +1,55 @@
+import 'package:date_format/date_format.dart';
+
+class SpanishLocale implements Locale {
+  const SpanishLocale();
+
+  final List<String> monthsShort = const [
+    'Ene',
+    'Feb',
+    'Mar',
+    'Abr',
+    'May',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Oct',
+    'Nov',
+    'Dic'
+  ];
+
+  final List<String> monthsLong = const [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Setiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ];
+
+  final List<String> daysShort = const [
+    'Lun',
+    'Mar',
+    'Mié',
+    'Jue',
+    'Vie',
+    'Sáb',
+    'Dom'
+  ];
+
+  final List<String> daysLong = const [
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado',
+    'Domingo'
+  ];
+}
