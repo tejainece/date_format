bf3cec7ceea7920d4a3ddf1054edb59486262680
Lucas Sena <slsena45@gmail.com>
2020-02-06 00:50:37 -0300

    Add support portuguese locale

---
+++ lib/src/locale/portuguese.dart
@@ -0,0 +1,55 @@
+import 'package:date_format/date_format.dart';
+
+class PortugueseLocale implements Locale {
+  const PortugueseLocale();
+
+  final List<String> monthsShort = const [
+    'Jan',
+    'Fev',
+    'Mar',
+    'Abr',
+    'Mai',
+    'Jun',
+    'Jul',
+    'Ago',
+    'Set',
+    'Out',
+    'Nov',
+    'Dez'
+  ];
+
+  final List<String> monthsLong = const [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ];
+
+  final List<String> daysShort = const [
+    'Seg',
+    'Ter',
+    'Qua',
+    'Qui',
+    'Sex',
+    'Sáb',
+    'Dom'
+  ];
+
+  final List<String> daysLong = const [
+    'Segunda-feira',
+    'Terça-feira',
+    'Quarta-feira',
+    'Quinta-feira',
+    'Sexta-feira',
+    'Sábado',
+    'Domingo'
+  ];
+}
