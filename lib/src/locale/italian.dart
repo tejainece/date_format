1a5d1bcd5df81d08ab6c08401376fecf3429f3b6
Hitsu91 <lucafang@hotmail.it>
2020-11-06 10:06:08 +0100

    added italian locale

---
+++ lib/src/locale/italian.dart
@@ -0,0 +1,61 @@
+import 'package:date_format/date_format.dart';
+
+class ItalianLocale implements Locale {
+  const ItalianLocale();
+
+  final List<String> monthsShort = const [
+    'Gen',
+    'Feb',
+    'Mar',
+    'Apr',
+    'Mag',
+    'Giu',
+    'Lug',
+    'Ago',
+    'Set',
+    'Ott',
+    'Nov',
+    'Dic'
+  ];
+
+  final List<String> monthsLong = const [
+    'Gennaio',
+    'Febbraio',
+    'Marzo',
+    'Aprile',
+    'Maggio',
+    'Giugno',
+    'Luglio',
+    'Agosto',
+    'Settembre',
+    'Ottobre',
+    'Novembre',
+    'Dicembre'
+  ];
+
+  final List<String> daysShort = const [
+    'Lun',
+    'Mar',
+    'Mer',
+    'Gio',
+    'Ven',
+    'Sab',
+    'Dom'
+  ];
+
+  final List<String> daysLong = const [
+    'Lunedì',
+    'Martedì',
+    'Mercoledì',
+    'Giovedì',
+    'Venerdì',
+    'Sabato',
+    'Domenica'
+  ];
+
+  @override
+  String get am => "AM";
+
+  @override
+  String get pm => "PM";
+}
