1255249018e44ad97c42658fcbe48789deefd757
Serdar Murat Çelik <serdar_murat@bitbucket.org>
2021-01-13 04:43:31 +0300

    added locale support for Turkish

---
+++ lib/src/locale/turkish.dart
@@ -0,0 +1,61 @@
+import 'package:date_format/date_format.dart';
+
+class TurkishLocale implements Locale {
+  const TurkishLocale();
+
+  final List<String> monthsShort = const [
+    'Oca',
+    'Şub',
+    'Mar',
+    'Nis',
+    'May',
+    'Haz',
+    'Tem',
+    'Ağu',
+    'Eyl',
+    'Eki',
+    'Kas',
+    'Ara'
+  ];
+
+  final List<String> monthsLong = const [
+    'Ocak',
+    'Şubat',
+    'Mart',
+    'Nisan',
+    'Mayıs',
+    'Haziran',
+    'Temmuz',
+    'Ağustos',
+    'Eylül',
+    'Ekim',
+    'Kasım',
+    'Aralık'
+  ];
+
+  final List<String> daysShort = const [
+    'Pzt',
+    'Sal',
+    'Çar',
+    'Per',
+    'Cum',
+    'Cts',
+    'Paz'
+  ];
+
+  final List<String> daysLong = const [
+    'Pazartesi',
+    'Salı',
+    'Çarşamba',
+    'Perşembe',
+    'Cuma',
+    'Cumartesi',
+    'Pazar'
+  ];
+
+  @override
+  String get am => "ÖÖ";
+
+  @override
+  String get pm => "ÖS";
+}
