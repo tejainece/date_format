94c7450e3b492272a973d21e782f2fe4a6ddac54
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-07-09 11:29:27 +0900

    Create korean.dart

---
+++ lib/src/locale/korean.dart
@@ -0,0 +1,61 @@
+import 'package:date_format/date_format.dart';
+
+class KoreanDateLocale implements DateLocale {
+  const KoreanDateLocale();
+
+  final List<String> monthsShort = const [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월'
+  ];
+
+  final List<String> monthsLong = const [
+    '1월',
+    '2월',
+    '3월',
+    '4월',
+    '5월',
+    '6월',
+    '7월',
+    '8월',
+    '9월',
+    '10월',
+    '11월',
+    '12월'
+  ];
+
+  final List<String> daysShort = const [
+    '월',
+    '화',
+    '수',
+    '목',
+    '금',
+    '토',
+    '일'
+  ];
+
+  final List<String> daysLong = const [
+    '월',
+    '화',
+    '수',
+    '목',
+    '금',
+    '토',
+    '일'
+  ];
+
+  @override
+  String get am => "오전";
+
+  @override
+  String get pm => "오후";
+}
